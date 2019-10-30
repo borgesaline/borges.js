aad281e79d761746e5fa43370821dac9b4856a89
borgesaline <aline_borges@msn.com>
2019-10-30 16:47:45 -0300

    resolvi exercicio ajax-b

--- 01b_ajax_json/index.js
+++ 01b_ajax_json/index.js
@@ -1,3 +1,29 @@
 // [] iterar sobre o array e mostrar as três imagens usando for()
 // [] iterar sobre o array e mostrar as três imagens usando for...of
 // [] iterar sobre o array e mostrar as três imagens usando forEach
+
+const div = document.querySelector('#root')
+
+const request = new XMLHttpRequest()
+const url = './data/dados.json'
+
+request.onreadystatechange = pegaDados
+
+request.open('GET', url)
+request.send()
+
+function pegaDados() {
+    if (request.readyState === 4 && request.status === 200) {
+        const response = request.response
+        const json = JSON.parse(response)
+
+        div.innerHTML = `   
+        <img src=${json.data[0].imagem}>
+        <img src=${json.data[1].imagem}>
+        <img src=${json.data[2].imagem}>
+        `
+    } else {
+        console.log('Errouuuu');
+        
+    }
+}
